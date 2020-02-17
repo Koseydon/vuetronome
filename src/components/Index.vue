cfcb149dffb082d241bd3d7295577d7a5cecf1fd
Koseydon <denizhasirci@icloud.com>
2020-02-17 16:31:15 +0300

    Metronome component without sound

---
+++ src/components/Index.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-card dark class="mx-auto" max-width="600">
+    <v-toolbar flat dense>
+      <v-toolbar-title>
+        <span class="subheading">VUETRONOME</span>
+      </v-toolbar-title>
+    </v-toolbar>
+    <v-card-text>
+      <v-row class="mb-4" justify="space-between">
+        <v-col class="text-left">
+          <span class="display-3 font-weight-light" v-text="bpm"></span>
+          <span class="subheading font-weight-light mr-1">BPM</span>
+          <v-fade-transition>
+            <v-avatar v-if="isPlaying" :color="color" :style="{
+                animationDuration: animationDuration
+              }" class="mb-1 v-avatar--metronome" size="12"></v-avatar>
+          </v-fade-transition>
+        </v-col>
+        <v-col class="text-right">
+          <v-btn :color="color" dark depressed fab @click="toggle">
+            <v-icon large>
+              {{ isPlaying ? 'mdi-pause' : 'mdi-play' }}
+            </v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+
+      <v-slider v-model="bpm" :color="color" track-color="grey" always-dirty min="40" max="218">
+        <template v-slot:prepend>
+          <v-icon :color="color" @click="decrement">
+            mdi-minus
+          </v-icon>
+        </template>
+
+        <template v-slot:append>
+          <v-icon :color="color" @click="increment">
+            mdi-plus
+          </v-icon>
+        </template>
+      </v-slider>
+    </v-card-text>
+  </v-card>
+</template>
+
+
+<script>
+  //import metronome from '../components/metronome'
+
+  export default {
+    data: () => ({
+      bpm: 40,
+      interval: null,
+      isPlaying: false,
+      clickAudio: new Audio('../audio/beat.wav')
+    }),
+
+    computed: {
+      color() {
+        if (this.bpm < 100) return 'indigo'
+        if (this.bpm < 125) return 'teal'
+        if (this.bpm < 140) return 'green'
+        if (this.bpm < 175) return 'orange'
+        return 'red'
+      },
+      animationDuration() {
+        return `${60 / this.bpm}s`
+      },
+    },
+
+    methods: {
+      decrement() {
+        this.bpm--
+      },
+      increment() {
+        this.bpm++
+      },
+      toggle() {
+        this.isPlaying = !this.isPlaying
+      },
+      soundOn() {
+        this.clickAudio.play()
+      }
+    },
+  }
+</script>
+
+<style>
+  @keyframes metronome-example {
+    from {
+      transform: scale(.5);
+    }
+
+    to {
+      transform: scale(1);
+    }
+  }
+
+  .v-avatar--metronome {
+    animation-name: metronome-example;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+  }
+</style>
